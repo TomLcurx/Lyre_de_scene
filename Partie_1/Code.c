3a3ebfe314b8e10670e4b2d11b2552324aceba77
TomLcurx <125253213+TomLcurx@users.noreply.github.com>
2025-09-18 21:08:30 +0200

    Update Code.c

--- Partie_1/Code.c
+++ Partie_1/Code.c
@@ -1,1 +1,41 @@
+const int PIN_DIR_Hor 18;
+const int PIN_PUL_Hor 19;
+const int PIN_DIR_Ver 22;
+const int PIN_PUL_Ver 23;
 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_DIR_Hor, OUTPUT);
+  pinMode(PIN_PUL_Hor, OUTPUT);
+  pinMode(PIN_DIR_Ver, OUTPUT);
+  pinMode(PIN_PUL_Ver, OUTPUT);
+
+  delayMicroseconds(5);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_DIR_Hor,LOW);
+  digitalWrite(PIN_DIR_Ver,LOW);
+
+  for(int i=0; i<max_pas; i++){
+    digitalWrite(PIN_PUL_Hor,LOW);
+    delayMicroseconds(10);
+    digitalWrite(PIN_PUL_Hor,HIGH);
+    delayMicroseconds(10);
+  }
+
+  delay(2000);
+
+    for(int i=0; i<max_pas; i++){
+    digitalWrite(PIN_PUL_Ver,LOW);
+    delayMicroseconds(10);
+    digitalWrite(PIN_PUL_Ver,HIGH);
+    delayMicroseconds(10);
+  }
+  
+  delay(2000);
+
+}
